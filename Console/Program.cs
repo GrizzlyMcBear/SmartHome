5e82fec4e3c0e58804ca5b63eab8759b8dc79782
Ronen Gershon Cohen <sronenco@gmail.com>
2020-06-04 22:25:14 +0300

    1. Added action items in the `README.md` file. 2. Added some test functionality under `Console` project, in `Program.cs`.

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 using System.Speech.Synthesis;
 using SmartHome.Models;
 using static System.Console;
+using System.Globalization;
+
 namespace Console_NetFramework
 {
 	class Program
 	{
-		static void Main(string[] args)
+		private static Home CreateHome()
 		{
-			Home home = new HomeBuilder()
+			return new HomeBuilder()
 				.SetFamilyName("Doe")
 				.AddFamilyMember("John")
 				.AddFamilyMember("Jane")
@@ -23,14 +25,65 @@ namespace Console_NetFramework
 				.AddDevice(new Device("Electric window #2"))
 				.AddDevice(new Device("Electric window #3"))
 				.RetrieveHome();
+		}
+
+		private static void PrintInstalledLanguages(SpeechSynthesizer synthesizer)
+		{
+			foreach (var voice in synthesizer.GetInstalledVoices())
+			{
+				var info = voice.VoiceInfo;
+				WriteLine($"Id: {info.Id} | Name: {info.Name} | Age: { info.Age} | Gender: { info.Gender} | Culture: { info.Culture}");
+			}
+		}
+
+		private static void SpeakMultiLanguageSentence(SpeechSynthesizer synthesizer)
+		{
+			synthesizer.SetOutputToDefaultAudioDevice();
+			var builder = new PromptBuilder();
+			builder.StartVoice(new CultureInfo("en-US"));
+			builder.AppendText("All we need to do is to keep talking.");
+			builder.EndVoice();
+			builder.StartVoice(new CultureInfo("ru-RU"));
+			builder.AppendText("Всё, что нам нужно сделать, это продолжать говорить");
+			builder.EndVoice();
+
+			synthesizer.Speak(builder);
+		}
+
+		private static void Testing_AppendTextWithHint(SpeechSynthesizer synthesizer)
+		{
+			var builder = new PromptBuilder();
+			builder.AppendTextWithHint("3rd", SayAs.NumberOrdinal);
+			builder.AppendBreak();
+			builder.AppendTextWithHint("3rd", SayAs.NumberCardinal);
+			builder.AppendBreak();
+			builder.AppendBookmark("First bookmark");
+			builder.AppendBreak();
+			builder.AppendTextWithPronunciation("DuBois", "duˈbwɑ");
+			builder.AppendBreak();
+			
+			synthesizer.Speak(builder);
+		}
+
+		static void Main(string[] args)
+		{
+			Home home = CreateHome();
 
 			// Print home details
-			WriteLine(home);
+			//WriteLine(home);
 
-			// Speak home details
 			var synthesizer = new SpeechSynthesizer();
-			synthesizer.SetOutputToDefaultAudioDevice();
-			synthesizer.Speak(home.ToString());
+
+			// Speak the home's details
+			//synthesizer.SetOutputToDefaultAudioDevice();
+			//synthesizer.SpeakAsync(home.ToString());
+
+			//PrintInstalledLanguages(synthesizer);
+			//SpeakMultiLanguageSentence(synthesizer);
+			//Testing_AppendTextWithHint(synthesizer);
+
+			Write("Press `Enter` to continue...");
+			ReadLine();
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # SmartHome
 A POC project for a smart home system, features would be added incrementally and would love to get feedbacks and suggestions
 
+
 ## Action Items
 
 - [ ] Add a POC for a feature to recognize speech and convert it into string
 - [ ] Design and implement a basic mechanism to turn that string into actions  
-i.e. add a new device to the home.
+i.e. add a new device to the home.  
+- [ ] Add image recognition features to open the pet door when recognizing our pets.  
+- [ ] Check the option of using the image recognition feature to unlock doors for family members and friends.
+
+
+## Links
+
+- [An MSDN article about TTS synthesis (June 2019)](https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/june/speech-text-to-speech-synthesis-in-net)
