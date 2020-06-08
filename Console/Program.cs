eeaeee8db4233ec85b2cf98ca19c204fa65158c7
Ronen Gershon Cohen <sronenco@gmail.com>
2020-06-08 15:30:41 +0300

    Refactored code by: 1. Adding `Speak` and `SpeakAsync` methods 2. Adding code from an article to recognize speech as a POC

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Speech.Synthesis;
+using System.Speech.Recognition;
 using SmartHome.Models;
 using static System.Console;
 using System.Globalization;
@@ -12,6 +13,20 @@ namespace Console_NetFramework
 {
 	class Program
 	{
+		#region Members
+		
+		private static SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+		private static SpeechRecognitionEngine speechRecognitionEngine = new SpeechRecognitionEngine();
+
+		static bool done = false;
+		
+		static bool speechOn = true;
+
+		private static Home home;
+
+		#endregion Members
+
 		private static Home CreateHome()
 		{
 			return new HomeBuilder()
@@ -27,7 +42,40 @@ namespace Console_NetFramework
 				.RetrieveHome();
 		}
 
-		private static void PrintInstalledLanguages(SpeechSynthesizer synthesizer)
+		private static void Initialize()
+		{
+			home = CreateHome();
+
+			synthesizer.SetOutputToDefaultAudioDevice();
+		}
+
+		#region TTS
+
+		private static PromptBuilder GetPromptBuilderForTTS(string tts, string cultureInfo)
+		{
+			var promptBuilder = new PromptBuilder();
+			promptBuilder.StartVoice(new CultureInfo(cultureInfo));
+			promptBuilder.AppendText(tts);
+			promptBuilder.EndVoice();
+
+			return promptBuilder;
+		}
+
+		private static void Speak(string tts, string cultureInfo = "en-US")
+		{
+			synthesizer.Speak(GetPromptBuilderForTTS(tts, cultureInfo));
+		}
+
+		private static void SpeakAsync(string tts, string cultureInfo = "en-US")
+		{
+			synthesizer.SpeakAsync(GetPromptBuilderForTTS(tts, cultureInfo));
+		}
+
+		#endregion TTS
+
+		#region TTS Synthesis Testing
+
+		private static void PrintInstalledLanguages()
 		{
 			foreach (var voice in synthesizer.GetInstalledVoices())
 			{
@@ -36,7 +84,7 @@ namespace Console_NetFramework
 			}
 		}
 
-		private static void SpeakMultiLanguageSentence(SpeechSynthesizer synthesizer)
+		private static void SpeakMultiLanguageSentence()
 		{
 			synthesizer.SetOutputToDefaultAudioDevice();
 			var builder = new PromptBuilder();
@@ -50,7 +98,7 @@ namespace Console_NetFramework
 			synthesizer.Speak(builder);
 		}
 
-		private static void Testing_AppendTextWithHint(SpeechSynthesizer synthesizer)
+		private static void Testing_AppendTextWithHint()
 		{
 			var builder = new PromptBuilder();
 			builder.AppendTextWithHint("3rd", SayAs.NumberOrdinal);
@@ -65,22 +113,128 @@ namespace Console_NetFramework
 			synthesizer.Speak(builder);
 		}
 
+		#endregion TTS Synthesis Testing
+
+		#region Speech Recognition
+
+		private static void InitializeSpeechRecognition()
+		{
+			speechRecognitionEngine.SetInputToDefaultAudioDevice();
+			speechRecognitionEngine.SpeechRecognized += OnSpeechRecognized;
+
+			Choices basicControlCommandChoices = new Choices();
+			basicControlCommandChoices.Add("speech on");
+			basicControlCommandChoices.Add("speech off");
+			basicControlCommandChoices.Add("test");
+			basicControlCommandChoices.Add("exit application");
+			basicControlCommandChoices.Add("exit app");
+
+			GrammarBuilder basicControlGrammarBuilder = new GrammarBuilder();
+			basicControlGrammarBuilder.Append(basicControlCommandChoices);
+			Grammar basicControlGrammar = new Grammar(basicControlGrammarBuilder);
+			
+			Choices digitChoices = new Choices();
+			digitChoices.Add("1");
+			digitChoices.Add("2");
+			digitChoices.Add("3");
+			digitChoices.Add("4");
+			digitChoices.Add("5");
+			digitChoices.Add("6");
+			digitChoices.Add("7");
+			digitChoices.Add("8");
+			digitChoices.Add("9");
+			digitChoices.Add("0");
+
+			GrammarBuilder additionGrammarBuilder = new GrammarBuilder();
+			additionGrammarBuilder.Append("What is");
+			additionGrammarBuilder.Append("How much is");
+			additionGrammarBuilder.Append(digitChoices);
+			additionGrammarBuilder.Append("plus");
+			additionGrammarBuilder.Append(digitChoices);
+			Grammar additionGrammar = new Grammar(additionGrammarBuilder);
+
+			speechRecognitionEngine.LoadGrammarAsync(basicControlGrammar);
+			speechRecognitionEngine.LoadGrammarAsync(additionGrammar);
+			
+			speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+		}
+
+		private static void SpeechRecognition()
+		{
+			try
+			{
+				InitializeSpeechRecognition();
+				while (done == false) {; }
+			}
+			catch (Exception ex)
+			{
+				WriteLine($"Exception encountered: {ex.Message}");
+			}
+		}
+
+		private static void OnSpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+		{
+			string txt = e.Result.Text;
+			float confidence = e.Result.Confidence;
+			
+			WriteLine("\nRecognized: " + txt);
+
+			if (confidence < 0.60) return;
+			
+			if (txt.IndexOf("speech on") >= 0)
+			{
+				WriteLine("Speech is now ON");
+				speechOn = true;
+			}
+			if (txt.IndexOf("speech off") >= 0)
+			{
+				WriteLine("Speech is now OFF");
+				speechOn = false;
+			}
+			
+			if (speechOn == false) return;
+			
+			if (txt.IndexOf("exit app") >= 0 || txt.IndexOf("exit application") >= 0)
+			{
+				((SpeechRecognitionEngine)sender).RecognizeAsyncCancel();
+				done = true;
+				WriteLine("Exiting application...");
+				Speak("Farewell");
+			}
+			if (txt.IndexOf("test") >= 0)
+			{
+				WriteLine("Testing");
+				Speak("Testing");
+			}
+			if (txt.IndexOf("What") >= 0 && txt.IndexOf("plus") >= 0)
+			{
+				string[] words = txt.Split(' ');
+				int num1 = int.Parse(words[2]);
+				int num2 = int.Parse(words[4]);
+				int sum = num1 + num2;
+				WriteLine("(Speaking: " + words[2] + " plus " + words[4] + " equals " + sum + ")");
+				SpeakAsync(words[2] + " plus " + words[4] + " equals " + sum);
+			}
+		}
+
+		#endregion Speech Recognition
+
 		static void Main(string[] args)
 		{
-			Home home = CreateHome();
+			Initialize();
 
 			// Print home details
 			//WriteLine(home);
-
-			var synthesizer = new SpeechSynthesizer();
 
 			// Speak the home's details
 			//synthesizer.SetOutputToDefaultAudioDevice();
 			//synthesizer.SpeakAsync(home.ToString());
 
-			//PrintInstalledLanguages(synthesizer);
-			//SpeakMultiLanguageSentence(synthesizer);
-			//Testing_AppendTextWithHint(synthesizer);
+			//PrintInstalledLanguages();
+			//SpeakMultiLanguageSentence();
+			//Testing_AppendTextWithHint();
+
+			SpeechRecognition();
 
 			Write("Press `Enter` to continue...");
 			ReadLine();
