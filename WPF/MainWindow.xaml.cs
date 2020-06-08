80ba9b6daea703189fb70b30fd3b52ad35e1d162
Ronen Gershon Cohen <sronenco@gmail.com>
2020-06-08 09:34:02 +0300

    Added a WPF application with GUI, allowing the user to click on a button to hear the relevant text in his selected language.

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,44 @@ namespace WPF
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private const string en = "en-US";
+
+		private const string ru = "ru-RU";
+
+		private readonly IDictionary<string, string> _messagesByCulture = new Dictionary<string, string>();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			PopulateMessages();
+		}
+
+		private void PromptInEnglish(object sender, RoutedEventArgs e)
+		{
+			DoPrompt(en);
+		}
+
+		private void PromptInRussian(object sender, RoutedEventArgs e)
+		{
+			DoPrompt(ru);
+		}
+
+		private void DoPrompt(string culture)
+		{
+			var synthesizer = new SpeechSynthesizer();
+			synthesizer.SetOutputToDefaultAudioDevice();
+			var builder = new PromptBuilder();
+			builder.StartVoice(new CultureInfo(culture));
+			builder.AppendText(_messagesByCulture[culture]);
+			builder.EndVoice();
+			//synthesizer.Speak(builder);
+			synthesizer.SpeakAsync(builder);
+		}
+
+		private void PopulateMessages()
+		{
+			_messagesByCulture[en] = "For the connection flight 123 to Saint Petersburg, please, proceed to gate A1";
+			_messagesByCulture[ru] = "Для пересадки на рейс 123 в  Санкт-Петербург, пожалуйста, пройдите к выходу A1";
 		}
 	}
 }
